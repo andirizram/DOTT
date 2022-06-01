61581a10750ce5f4ec8931a9d521dc81773a6431
120140002_AndikaRizkiRamadhan <95036143+andirizram@users.noreply.github.com>
2022-06-01 23:20:39 +0700

    menambahkan audio pada main menu

--- main_menu.py
+++ main_menu.py
@@ -1,8 +1,12 @@
 import pygame
 import pygame_menu
+import os
 
 pygame.init()
 pygame.display.set_caption('Defense Of The Tower (DOTT) TUBES PBO RB-08')
+pygame.mixer.music.load(os.path.join("Assets/audio", "main_menu.ogg"))
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 menu_utama = pygame.display.set_mode((800, 400))
 
 def mulai_game():
